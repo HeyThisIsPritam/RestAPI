e7c02f82fb1a602e96b4c65dac11fe7ae0f7244f
Pritam Biswas <prit.biswas1996@gmail.com>
2022-05-25 14:08:45 +0530

    Pushing DBfile

---
+++ EmployeeDAL/ScriptFile/EmployeeDB.sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE EmployeeDB;
+GO
+Use EmployeeDB;
+GO
+CREATE TABLE [dbo].[Employee] (
+    [empId]       INT          IDENTITY (1000, 1) NOT NULL,
+    [empName]     VARCHAR (20) NOT NULL,
+    [designation] VARCHAR (20) NOT NULL,
+    [DOJ]         DATE         NOT NULL,
+    PRIMARY KEY CLUSTERED ([empId] ASC)
+);
+GO
+INSERT INTO Employee VALUES('Pritam','Application Dev','2020-11-09');
+INSERT INTO Employee VALUES('Debaraj','Tech Lead','2016-07-19');
+INSERT INTO Employee VALUES('Shubasish','Group Manager','2002-11-09');
+GO
